56b33fd43f833f7d18395ce8a9fda6c5fae9ba8c
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-02-18 17:43:08 +0100

    cambios en java para modal

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,10 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    const btnAbrirModal = document.querySelector("#btn-abrir-modal");
+    const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+    const modal = document.querySelector("#modal");
+
+    btnAbrirModal.addEventListener("click",()=>{ modal.show() })
+    btnCerrarModal.addEventListener("click",()=>{ modal.close() })
 });
