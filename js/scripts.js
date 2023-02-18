9334b021ff08155dac83c567d3697eac6332718f
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2023-02-18 19:06:44 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,10 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-const btnAbrirModal = document.querySelector("#btn-abrir-modal");
-const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
-const modal = document.querySelector("#modal1");
-
-btnAbrirModal.addEventListener("click",()=>{ modal.showModal() })
-btnCerrarModal.addEventListener("click",()=>{ modal.close() })
